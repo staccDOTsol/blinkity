06d82ce659bb1b470546fdeeff13d69abe302a47
Jarett Dunn <jarettdunn@Jaretts-Laptop.local>
2024-07-04 08:13:06 +0100

    redundant imports relieved; until such time as token groups

--- examples/index.ts
+++ examples/index.ts
@@ -36,7 +36,7 @@ app.get(
   }),
 );
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 console.log(
   `Server is running on port ${port}
 Visit http://localhost:${port}/swagger-ui to explore existing actions
